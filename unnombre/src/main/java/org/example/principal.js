4807dac029e45d0837c8142ae7b4aabe3ad432d0
veronica-7 <vettonica18@gmail.com>
2024-01-17 15:04:49 -0500

    -acommit3

--- unnombre/src/main/java/org/example/principal.js
+++ unnombre/src/main/java/org/example/principal.js
@@ -1,7 +1,6 @@
-
 /*// Importar RxJS
 const { from } = require('rxjs');
-const { filter, map, scan, concatoncatMap }= require('rxjs/operators');
+const { filter, map, scan, concatMap }= require('rxjs/operators');
 
 // Crear un array de números
 const lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -26,26 +25,46 @@ const { from } = require('rxjs');
 const { filter, map, scan, concat, delay, concatMap } = require('rxjs');
 
 // Arrays de números
-const lista1 = [5, 8, 2, 0, 4];
-const lista2 = [2, 1, 7, 8, 1];
+const lista1 = ["A", "B", "C", "D", "F"];
+const lista2 = ["Andrés", "Bernardo", "Camila", "Daniela"];
+const lista3 = [5, 6, 4, 8, 9, 3, 2];
+const lista4 = [4, 6, 0, 9];
+
+const obsConcatenar = concat(from(lista1), from(lista2), from(lista4));
+obsConcatenar.subscribe(r => {
+    console.log(`Resultado de la concatenación: ${r}`);
+});
 
 // Crear dos Observables desde los arrays
 // const observable1 = from(array1);
 // const observable2 = from(array2);
 
-const obs1 = from(lista1).pipe(concatMap(numero => from([numero]).pipe(delay(1000))));
+const obs1 = from(lista1).
+pipe(concatMap(numero => from([numero]).pipe(delay(1000))));
+const obs2 = from(lista2).
+pipe(concatMap(numero => from([numero]).pipe(delay(2000))));
 
-const obs2 = from(lista2).pipe(concatMap(numero => from([numero]).pipe(delay(2000))));
-
-const resultante = concat(obs1, obs2);
+const concatenar = concat(obs1, obs2); // Use las dos listas de números
 
 // Suscribirse al Observable resultante
-resultante.subscribe(resultado => {
-    console.log(`Resultado de la concatenación: ${resultado}`);
-
+concatenar.subscribe(r => {
+    console.log(`Resultado de la concatenación 2: ${r}`);
 })
+console.log(`Concatenado 2 terminado!\n`)
 
-console.log(`Realizado`)
+
+const observableNums = from(lista3);
+
+const numsPar = observableNums.pipe(
+    filter(num => num % 2 === 0),  // Filtrar números pares
+    map(num => num * 2),  // Multiplicar cada número par por 2
+    scan((acc, num) => acc + num));  // Acumular la suma de los números
+
+numsPar.subscribe(r => {
+    console.log('La suma de los números pares es:', r);
+    console.log("Sumado!")
+})
+console.log(`Suma Finalizada...\n`)
 
 
 /*const { from, of} = require("rxjs");
